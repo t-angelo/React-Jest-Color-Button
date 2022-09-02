f82dc4e32581f99c09d1d0c80376d8c8decbc982
625610 <tsaganeas_angelo@bah.com>
2022-09-02 10:47:02 -0400

    Add label to button

--- src/App.test.js
+++ src/App.test.js
@@ -34,15 +34,14 @@ test('initial conditions', () => {
 
 test('Checkbox disables button on first click and enables on second click', () => {
   render(<App />)
-
-  const checkbox = screen.getByRole('checkbox')
-  const button = screen.getByRole('button')
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' })
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
   
   fireEvent.click(checkbox)
-  expect(button).toBeDisabled()
+  expect(colorButton).toBeDisabled()
   
   fireEvent.click(checkbox)
-  expect(button).toBeEnabled()  
+  expect(colorButton).toBeEnabled()  
 })
 
 
